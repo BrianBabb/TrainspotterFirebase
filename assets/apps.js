2043abb18a2e6bd1a5ef689aa0da8a48df5315ce
brian babb <babb.brian@gmail.com>
2019-10-24 22:14:04 -0400

    update after tutor

--- assets/apps.js
+++ assets/apps.js
@@ -1,36 +1,51 @@
 
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js"></script>
-
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js"></script>
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyABvymBW2eHUYcaSOXIoRF-iFMuRl2YDsA",
-    authDomain: "exampleprojectx1.firebaseapp.com",
-    databaseURL: "https://exampleprojectx1.firebaseio.com",
-    projectId: "exampleprojectx1",
-    storageBucket: "exampleprojectx1.appspot.com",
-    messagingSenderId: "1046056285857",
-    appId: "1:1046056285857:web:6ac6fa95c41346af9822e0",
-    measurementId: "G-8NQE2FHXHE"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
-
-
-
-
-
-
 
 $(document).ready(function() {
 
+
+// Web API Key
+// AIzaSyABvymBW2eHUYcaSOXIoRF-iFMuRl2YDsA
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyABvymBW2eHUYcaSOXIoRF-iFMuRl2YDsA",
+  authDomain: "exampleprojectx1.firebaseapp.com",
+  databaseURL: "https://exampleprojectx1.firebaseio.com",
+  projectId: "exampleprojectx1",
+  storageBucket: "exampleprojectx1.appspot.com",
+  messagingSenderId: "1046056285857",
+  appId: "1:1046056285857:web:6ac6fa95c41346af9822e0",
+  measurementId: "G-8NQE2FHXHE"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var database = firebase.database();
+
+
+console.log(firebaseConfig);
+
+$("#button").on("click", function(event){
+  console.log("hey this button is working");
+
+  event.preventDefault();
+
+  // grabs user input 
+  var trainName = $("#trainName").val().trim();
+
+  var entry = {
+  trainName: trainName,
+}
+console.log(trainName);
+
+database.ref().push(entry);
+})
+
+database.ref().on("child_added", function(childSnapshot) {
+console.log(childSnapshot.val());
+})
 
 
 
